3b506919f99caf8f5ac933919c95c5586854009c
Kevin March <kmarchtomassone@gmail.com>
2023-06-05 00:46:29 -0400

    nosepq pero el primer comit no tomo xdddd

--- TRIGGER2.sql
+++ TRIGGER2.sql
@@ -1,7 +1,6 @@
 CREATE FUNCTION user_no_compra_a_si_mismo() RETURNS trigger AS 
 $$
 	BEGIN
-<<<<<<< HEAD
 		IF EXISTS (
 		SELECT 1
 		FROM item_transacction
@@ -12,12 +11,10 @@ $$
 		RAISE EXCEPTION 'No se puede insertar o actualizar la transaccion debido a que un usuario no puede comprar su mismo item';
 	END IF; 
 	RETURN NEW;
-=======
 		IF EXISTS (SELECT 1 FROM item_transacction WHERE NEW.user_buyer_id = user_seller_id ORDER BY user_seller_id DESC LIMIT 1) THEN 
 			RAISE EXCEPTION 'No podes comprarte a ti mismo, no autorizo';
 		END IF; 
 		RETURN NEW;
->>>>>>> mercado
 	END;
 $$
 LANGUAGE plpgsql;
