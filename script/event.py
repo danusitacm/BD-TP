af8dc74e4d4280dbbf47cae9bbc6e79919ac27c4
danusitacm <danielacristaldomo@gmail.com>
2023-06-07 00:19:01 -0400

    ya genera eventos con fechas consecutivas y no repetitivas

--- script/event.py
+++ script/event.py
@@ -1,22 +1,26 @@
 from faker import Faker
 import random
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta,date
 n=10
 fake = Faker()
-name_event=('Festival de la Construcción de Bases',
+name_event=('Festival de la Construccion de Bases',
             'Next Fest de febrero','Festival del Misterio',
-            'Rebajas de Otoño', 'Rebajas de Invierno', 
+            'Rebajas de Otono', 'Rebajas de Invierno', 
             'Festival del sigilo', 
             'Rebajas de Primavera','Festival de los Puzles', 
             'Festival de los Deportes', 'Rebajas de Verano', 
-            'Festival del sigilo','Festival de los Deportes')
+            'Festival del sigilo','Festival de los Deportes','Racing Fest',
+            'Empanada don vito')
+
 def insert_into_event(db_manager):
+    start_date=date(2023, 1, 1)
     values=[]
     query="INSERT INTO event (name,description,start_date,end_date) VALUES (%s,%s,%s,%s)"
     for _ in range(n):
         name=random.choice(name_event)
         description=fake.text(max_nb_chars=300)
-        start_date=fake.date_between(start_date=datetime(2023,1,1), end_date='+30d')
-        end_date=fake.date_between(start_date=start_date, end_date=datetime(2023,12,31))
-        values.append((name,description,start_date,end_date))  
-    db_manager.execute_query(query, values)
+        random_start_date= start_date + timedelta(days=random.randint(1, 10))
+        random_end_date= random_start_date + timedelta(days=random.randint(1, 10))
+        start_date = random_end_date
+        values.append((name,description,random_start_date,random_end_date))  
+    #b_manager.execute_query(query, values)
