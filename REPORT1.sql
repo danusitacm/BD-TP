d519daf4a403785efcbafb0223ee5af3643b6f4c
danusitacm <danielacristaldomo@gmail.com>
2023-06-11 12:43:33 -0400

    Sin querer en algun commit modifique report1

--- REPORT1.sql
+++ REPORT1.sql
@@ -1,27 +1,14 @@
-CREATE FUNCTION calculo_precio_compra() RETURNS trigger AS
-$$
-	DECLARE
-		discount DECIMAL(10,2);
-   	    base_price DECIMAL(10,2);
-   	 	total_price DECIMAL(10,2);
-    BEGIN
-	SELECT g.base_price, ge.discount INTO base_price, discount
+CREATE OR REPLACE FUNCTION getGamesFromEvent(id bigint)
+   RETURNS TABLE (nameGame varchar)
+   LANGUAGE plpgsql
+AS $$
+BEGIN
+    RETURN QUERY
+    SELECT g.name
     FROM game g
-    JOIN game_event ge ON ge.game_id = g.game_id
-    JOIN event e ON e.event_id = ge.event_id
-    WHERE g.game_id = NEW.game_id
-      AND NEW.purchase_date BETWEEN e.start_date AND e.end_date;
-
-    -- Calcular el precio total
-	select base_price - (base_price * discount) into total_price;
-   
-    -- Actualizar el campo total_price en la tabla ubg
-    UPDATE user_buy_game SET total_price = total_price WHERE user_id = NEW.user_id AND purchase_id = NEW.purchase_id;
-  	RETURN NEW;
-    END;
-$$
-LANGUAGE plpgsql;
-CREATE TRIGGER calculo_precio_compra
-    AFTER INSERT OR UPDATE ON user_buy_game
-    FOR EACH ROW 
-    EXECUTE PROCEDURE calculo_precio_compra()
+    JOIN user_buy_game ubg ON g.game_id = ubg.game_id
+    WHERE ubg.purchase_date BETWEEN (SELECT start_date FROM event WHERE event_id = id)
+    AND (SELECT end_date FROM event WHERE event_id = id);
+    RETURN;
+END;
+$$;
