278359245f90d4c58a344088250ff94739a35a14
danusitacm <danielacristaldomo@gmail.com>
2023-06-10 23:02:44 -0400

    Actualizacion de insert_into_user_buy_game

--- script/game.py
+++ script/game.py
@@ -5,30 +5,27 @@ fake = Faker()
 def insert_into_game(db_manager):
     values=[]
     query="select developer_id from developer ORDER BY developer_id DESC LIMIT 1"
-    start_range=1
     print(start_range)
     end_range=db_manager.get_last_id_from_table(query)
-    print(end_range)
     query="INSERT INTO game (description,name,release_date,base_price,developer_id) VALUES (%s,%s,%s,%s,%s)"
     for _ in range(n):
         description=fake.text(max_nb_chars=300)
         name=fake.word()
         release_date=fake.date_this_year()
         base_price=random.randint(70000, 1000000) 
-        developer_id=random.randint(start_range, end_range)
+        developer_id=random.randint(1,end_range)
         values.append((description,name,release_date,base_price,developer_id))   
     db_manager.execute_query(query, values)
     
 def insert_into_user_buy_game(db_manager):
     values=[]
-    start_range=1
-    end_range=10
     query_last_user_id="select user_id from user_1 ORDER BY user_id DESC LIMIT 1"
-    query_last_game_id="select game_id from ga_1 ORDER BY user_id DESC LIMIT 1"
+    query_last_game_id="select game_id from game ORDER BY game_id DESC LIMIT 1"
     query="INSERT INTO user_buy_game (total_price,purchase_date,game_id,user_id) VALUES (%s,%s,%s,%s)"
-    total_price=random.uniform(start_range,end_range)  #este tenemos que cambiar#
-    purchase_date=fake.date_this_year()
-    game_id=random.randint(start_range,db_manager.get_last_id_from_table(query))
-    user_id=random.randint(start_range, end_range)
-    values.append((total_price,purchase_date,game_id,user_id))   
+    for _ in range(n):
+        total_price=0  #este tenemos que cambiar#
+        purchase_date=fake.date_this_year()
+        game_id=random.randint(1,db_manager.get_last_id_from_table(query_last_game_id))
+        user_id=random.randint(1,db_manager.get_last_id_from_table(query_last_user_id))
+        values.append((total_price,purchase_date,game_id,user_id))   
     db_manager.execute_query(query, values)
--- script/main.py
+++ script/main.py
@@ -1,6 +1,6 @@
 from database_manager import DatabaseManager
 from user import insert_into_user,insert_into_developer
-from game import insert_into_game
+from game import insert_into_game,insert_into_user_buy_game
 from event import insert_into_event
 from game_event import insert_into_game_event
 
@@ -8,8 +8,10 @@ if __name__ == "__main__":
     db_manager = DatabaseManager(user="postgres", password="12345", host="127.0.0.1", port="5432", database="bd")
     db_manager.connect()
     #insert_into_event(db_manager)
+    #insert_into_user(db_manager)
     #insert_into_developer(db_manager)
     #insert_into_game(db_manager)
-    insert_into_game_event(db_manager)
+    #insert_into_game_event(db_manager)
+    #insert_into_user_buy_game(db_manager)
     db_manager.disconnect()
     
