ce54b2242cdc1a3c2dfe7da250d60601fba7e99a
danusitacm <danielacristaldomo@gmail.com>
2023-06-13 22:53:54 -0400

    Modificacion y testeo de rarity,producto, item

--- script/items.py
+++ script/items.py
@@ -1,31 +1,34 @@
 from faker import Faker
 import random
-n=10
+n=30
 fake = Faker()
 def insert_into_products(db_manager):
     values=[]
     start_range=1
     end_range=10
-    query="INSERT INTO product (product_name,product_price,product_description,game_id,rarity_id) VALUES (%s,%s,%s,%s,%s)"
+    query="INSERT INTO product (product_name,producto_price,product_description,game_id,rarity_id) VALUES (%s,%s,%s,%s,%s)"
+    query_last_game_id="select game_id from game ORDER BY game_id DESC LIMIT 1"
+    last_id_game=db_manager.get_last_id_from_table(query_last_game_id)
     for _ in range(n):
         product_name=fake.name()
         product_price=random.uniform(start_range, end_range)
         product_description=fake.text(max_nb_chars=300)
-        game_id=random.randint(start_range, end_range)
+        game_id=random.randint(1,last_id_game)
         rarity_id=random.randint(start_range, 5)
         values.append((product_name,product_price,product_description,game_id,rarity_id))   
     db_manager.executemany_query(query, values)
 def insert_into_items(db_manager):
     estados = ['Activo', 'Inactivo', 'Pendiente', 'Finalizado']
-    indice_aleatorio = random.randint(0, len(estados) - 1)
     values=[]
-    start_range=1
-    end_range=10
-    query="INSERT INTO items (drop_date,user_id,product_acquired_id,item_status) VALUES (%s,%s,%s,%s)"
+    query_last_user_id="select user_id from user_1 ORDER BY user_id DESC LIMIT 1"
+    last_id_user=db_manager.get_last_id_from_table(query_last_user_id)
+    query_last_product_id="select product_id from product ORDER BY product_id DESC LIMIT 1"
+    last_id_product=db_manager.get_last_id_from_table(query_last_product_id)
+    query="INSERT INTO item (drop_date,user_id,product_acquired_id,item_status) VALUES (%s,%s,%s,%s)"
     for i in range(n):
         drop_date= fake.date_this_year()
-        user_id= random.randint(start_range, end_range)
-        product_acquired_id= random.randint(start_range, end_range)
-        item_status= estados[indice_aleatorio]
+        user_id=random.randint(1, last_id_user)
+        product_acquired_id= random.randint(1, last_id_product)
+        item_status=random.choice(estados)
         values.append((drop_date,user_id,product_acquired_id,item_status))
     db_manager.executemany_query(query, values)
--- script/main.py
+++ script/main.py
@@ -7,6 +7,8 @@ from review_game import insert_into_game_review
 from community import insert_into_community
 from community_user import insert_into_community_user
 from gift import insert_into_gift
+from items import insert_into_items, insert_into_products
+from rarity import insert_into_rarity
 if __name__ == "__main__":
     db_manager = DatabaseManager(user="postgres", password="12345", host="127.0.0.1", port="5432", database="bd")
     db_manager.connect()
@@ -16,9 +18,12 @@ if __name__ == "__main__":
     #insert_into_game(db_manager)
     #insert_into_game_event(db_manager)
     #insert_into_user_buy_game(db_manager)
-    insert_into_game_review(db_manager)
+    #insert_into_game_review(db_manager)
     #insert_into_community(db_manager)
     #insert_into_community_user(db_manager)
     #insert_into_gift(db_manager)
+    #insert_into_rarity(db_manager)
+    #insert_into_products(db_manager)
+    insert_into_items(db_manager)
     db_manager.disconnect()
     
