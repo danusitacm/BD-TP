1ed1e057e3c82215b24fb6049aa1f364f4acf302
danusitacm <danielacristaldomo@gmail.com>
2023-06-14 00:29:57 -0400

    arreglo, testeo y mejora de gift

--- script/gift.py
+++ script/gift.py
@@ -1,25 +1,23 @@
 from faker import Faker
 import random
-n=200000
 fake = Faker()
 def insert_into_gift(db_manager):
+    n=10
     values=[]
-    query_last_user_id="select user_id from user_1 ORDER BY user_id DESC LIMIT 1"
-    last_id_user=db_manager.get_last_id_from_table(query_last_user_id)
-    query_last_game_id="select game_id from game ORDER BY game_id DESC LIMIT 1"
-    last_id_game=db_manager.get_last_id_from_table(query_last_game_id)
-    query_last_community_id="select community_id from community ORDER BY community_id DESC LIMIT 1"
-    last_id_community=db_manager.get_last_id_from_table(query_last_community_id)
     query="INSERT INTO gift (shipping_date,user_id,game_gift_id,community_gift_id) VALUES (%s,%s,%s,%s)"
+    query_last_id_purchase="SELECT user_buy_game FROM user_buy_game ORDER BY user_buy_game DESC LIMIT 1"
+    query_last_id_community="select community_id from community ORDER BY community_id DESC LIMIT 1"
+    last_id_community=db_manager.get_last_id_from_table(query_last_id_community)
+    last_id_purchase=db_manager.get_last_id_from_table(query_last_id_purchase)
     for _ in range(n):
         shipping_date=fake.date_this_year()
-        user_id=random.randint(1,last_id_user)
-        game_gift_id=random.randint(1,last_id_game)
         community_gift_id=random.randint(1,last_id_community)
-       #user_has_game = db_manager.check_user_has_game(user_id,game_id)
-        """ if not user_has_game:
-            print(f"El usuario {user_id} no tiene el juego con ID {game_id}. No se insertará la reseña.")
-            continue
-        """
-        values.append((shipping_date,user_id,game_gift_id,community_gift_id))   
-    db_manager.execute_query(query, values)
+        id_purchase=random.randint(1,last_id_purchase)
+        query_purchase = f"SELECT user_id, game_id FROM user_buy_game WHERE user_buy_game={id_purchase} LIMIT 1"
+        purchase_data = db_manager.get_data(query_purchase)
+        if purchase_data:
+            print(purchase_data)
+            user_id=purchase_data[0][0]
+            game_gift_id= purchase_data[0][1]
+            values.append((shipping_date,user_id,game_gift_id,community_gift_id))   
+    db_manager.executemany_query(query, values)
--- script/review_game.py
+++ script/review_game.py
@@ -4,18 +4,18 @@ fake = Faker()
 def insert_into_game_review(db_manager):
     n = 10000
     values=[]
+    query_purchase = "SELECT user_id, game_id FROM user_buy_game ORDER BY RANDOM() LIMIT 1"
+    query = "INSERT INTO game_review (description, publish_date, score, title, game_id, user_id) VALUES (%s, %s, %s, %s, %s, %s)"
     for i in range(n):
         description = generate_random_description()
         publish_date = fake.date_this_year()
         score = random.randint(1, 10)
         title = generate_random_title()
         # Obtener una compra aleatoria de la tabla purchase para un usuario que ha comprado un juego
-        query_purchase = "SELECT user_id, game_id FROM user_buy_game ORDER BY RANDOM() LIMIT 1"
         purchase_data = db_manager.get_data(query_purchase)
         if purchase_data:
             user_id=purchase_data[0][0]
             game_id= purchase_data[0][1]
-            query = "INSERT INTO game_review (description, publish_date, score, title, game_id, user_id) VALUES (%s, %s, %s, %s, %s, %s)"
             values.append((description, publish_date, score, title, game_id, user_id))
     db_manager.executemany_query(query, values)  # Ejecutar la consulta con los valores generados
 
