7567cb008615180923a870dc5fe829f90cf2250a
danusitacm <danielacristaldomo@gmail.com>
2023-06-29 21:31:56 -0400

    modifacion game.py, game_event.py

--- script/game.py
+++ script/game.py
@@ -1,6 +1,7 @@
 from faker import Faker
 import random
-n=200000
+from datetime import datetime, timedelta,date
+n=500000
 fake = Faker()
 def insert_into_game(db_manager):
     values=[]
@@ -15,18 +16,21 @@ def insert_into_game(db_manager):
         developer_id=random.randint(1,end_range)
         values.append((description,name,release_date,base_price,developer_id))   
     db_manager.executemany_query(query, values)
-    
+  
 def insert_into_user_buy_game(db_manager):
     values=[]
+    start_date=date(2023, 1, 1)
     query_last_user_id="select user_id from user_1 ORDER BY user_id DESC LIMIT 1"
     last_id_user=db_manager.get_last_id_from_table(query_last_user_id)
     query_last_game_id="select game_id from game ORDER BY game_id DESC LIMIT 1"
     last_id_game=db_manager.get_last_id_from_table(query_last_game_id)
     query="INSERT INTO user_buy_game (total_price,purchase_date,game_id,user_id) VALUES (%s,%s,%s,%s)"
-    for _ in range(n):
-        total_price=random.randint(70000, 1000000) 
-        purchase_date=fake.date_this_year()
-        game_id=random.randint(1,last_id_game)
-        user_id=random.randint(1,last_id_user)
-        values.append((total_price,purchase_date,game_id,user_id))   
+    for _ in range(5000):
+        for _ in range(100):
+            total_price=random.randint(70000, 1000000) 
+            purchase_date=start_date 
+            game_id=random.randint(1,last_id_game)
+            user_id=random.randint(1,last_id_user)
+            values.append((total_price,purchase_date,game_id,user_id))
+        start_date = purchase_date + timedelta(days=random.randint(0,1))
     db_manager.executemany_query(query, values)
--- script/game_event.py
+++ script/game_event.py
@@ -3,15 +3,13 @@ import random
 import time
 random.seed(int(time.time()) ) 
 def insert_into_game_event(db_manager):
-    n=500000
+    n=200000
     values=[]
     query_last_game_id="select game_id from game ORDER BY game_id DESC LIMIT 1"
     last_id_game=db_manager.get_last_id_from_table(query_last_game_id)
-    query_last_event_id="select event_id from event ORDER BY event_id DESC LIMIT 1"
-    last_event_id=db_manager.get_last_id_from_table(query_last_event_id)
     query="insert into game_event (event_id,game_id,discount) values (%s,%s,%s) ON CONFLICT DO NOTHING"
     for i in range(n):
-        event_id=random.randint(1,last_event_id)
+        event_id=random.randint(1,100)
         game_id=random.randint(1,last_id_game)
         discount=round(random.uniform(0.03, 0.8), 1)
         values.append((event_id,game_id,discount))
