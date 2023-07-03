99bbd0c2de281ec5dc36f35b95324a48ea86ba95
danusitacm <danielacristaldomo@gmail.com>
2023-07-03 17:35:34 -0400

    create 2 files and update 12 files update balance.py

--- script/balance.py
+++ script/balance.py
@@ -2,6 +2,7 @@ from librerias import *
 def insert_into_balance(db_manager):
     n=185000
     values=[]
+    metodos_de_pago = ['Tarjetas de crédito o débito', 'PayPal', 'Bitcoin', 'Tarjetas prepagadas']
     start_date=date(2023, 1, 1)
     query_last_user_id="select user_id from user_1 ORDER BY user_id DESC LIMIT 1"
     last_id_user=db_manager.get_last_id_from_table(query_last_user_id)
@@ -11,7 +12,7 @@ def insert_into_balance(db_manager):
             amount=random.randint(700000, 1000000) 
             transaction_date=start_date + timedelta(days=random.randint(0,1))
             description=fake.text(max_nb_chars=300)
-            payment_type=random.choice['Tarjetas de crédito o debito','PayPal','Bitcoin','Tarjetas prepagadas']
+            payment_type=random.choice(metodos_de_pago)
             user_balance_id=random.randint(1,last_id_user)
             values.append((amount, description, transaction_date, payment_type, user_balance_id))
         start_date = transaction_date + timedelta(days=random.randint(0,5))
