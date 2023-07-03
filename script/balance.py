2cf7cca36d31129f73782cb45b33f35a6a14e71b
danusitacm <danielacristaldomo@gmail.com>
2023-07-03 17:48:27 -0400

    update balance.cpython-311.pyc and balance.py

--- script/balance.py
+++ script/balance.py
@@ -4,16 +4,19 @@ def insert_into_balance(db_manager):
     values=[]
     metodos_de_pago = ['Tarjetas de crédito o débito', 'PayPal', 'Bitcoin', 'Tarjetas prepagadas']
     start_date=date(2023, 1, 1)
-    query_last_user_id="select user_id from user_1 ORDER BY user_id DESC LIMIT 1"
-    last_id_user=db_manager.get_last_id_from_table(query_last_user_id)
+    query_last_id_purchase="SELECT user_buy_game FROM user_buy_game ORDER BY user_buy_game DESC LIMIT 1"
+    last_id_purchase=db_manager.get_last_id_from_table(query_last_id_purchase)
     query="INSERT INTO balance (amount, description, transaction_date, payment_type, user_balance_id) VALUES (%s,%s,%s,%s,%s)"
-    for _ in range(1000):
+    for _ in range(10):
         for _ in range(185):
-            amount=random.randint(700000, 1000000) 
-            transaction_date=start_date + timedelta(days=random.randint(0,1))
             description=fake.text(max_nb_chars=300)
             payment_type=random.choice(metodos_de_pago)
-            user_balance_id=random.randint(1,last_id_user)
+            id_purchase=random.randint(1,last_id_purchase)
+            query_purchase = f"SELECT user_id, total_price, purchase_date FROM user_buy_game WHERE user_buy_game={id_purchase} LIMIT 1"
+            purchase_data = db_manager.get_data(query_purchase)
+            if purchase_data:
+                user_balance_id=purchase_data[0][0]
+                amount= purchase_data[0][1]
+                transaction_date=purchase_data[0][2]
             values.append((amount, description, transaction_date, payment_type, user_balance_id))
-        start_date = transaction_date + timedelta(days=random.randint(0,5))
     db_manager.executemany_query(query, values)
