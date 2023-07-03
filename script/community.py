269aa8d619d57fa58d0afab86567901111b75e36
danusitacm <danielacristaldomo@gmail.com>
2023-07-03 18:24:50 -0400

    update community.py

--- script/community.py
+++ script/community.py
@@ -1,11 +1,12 @@
 from librerias import *
-n=100000
-fake = Faker()
 def insert_into_community(db_manager):
-    values=[]
-    query="INSERT INTO community (name,description) VALUES (%s,%s)"
-    for _ in range(n):
-        name=fake.word()
-        description=fake.text(max_nb_chars=300)
-        values.append((name,description))   
-    db_manager.executemany_query(query, values)
+    num_communities = 100000
+    values = []
+    query_insert_community = "INSERT INTO community (name, description) VALUES (%s, %s)"
+    
+    for _ in range(num_communities):
+        name = fake.word()
+        description = fake.text(max_nb_chars=300)
+        values.append((name,description))
+    
+    db_manager.executemany_query(query_insert_community,values)
