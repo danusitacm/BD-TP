fe435081a53a699a3376db5db48a670140052875
danusitacm <danielacristaldomo@gmail.com>
2023-07-03 18:20:35 -0400

    update 2 files and copy 1 file

--- script/balance.py
+++ script/balance.py
@@ -1,22 +1,23 @@
 from librerias import *
 def insert_into_balance(db_manager):
-    n=185000
     values=[]
     metodos_de_pago = ['Tarjetas de crédito o débito', 'PayPal', 'Bitcoin', 'Tarjetas prepagadas']
     start_date=date(2023, 1, 1)
-    query_last_id_purchase="SELECT user_buy_game FROM user_buy_game ORDER BY user_buy_game DESC LIMIT 1"
-    last_id_purchase=db_manager.get_last_id_from_table(query_last_id_purchase)
-    query="INSERT INTO balance (amount, description, transaction_date, payment_type, user_balance_id) VALUES (%s,%s,%s,%s,%s)"
+    query_last_purchase_id="SELECT user_buy_game FROM user_buy_game ORDER BY user_buy_game DESC LIMIT 1"
+    query_insert_balance = "INSERT INTO balance (amount, description, transaction_date, payment_type, user_balance_id) VALUES (%s,%s,%s,%s,%s)"
+    last_purchase_id=db_manager.get_last_id_from_table(query_last_purchase_id)
     for _ in range(10):
         for _ in range(185):
             description=fake.text(max_nb_chars=300)
             payment_type=random.choice(metodos_de_pago)
-            id_purchase=random.randint(1,last_id_purchase)
+            id_purchase=random.randint(1,last_purchase_id)
             query_purchase = f"SELECT user_id, total_price, purchase_date FROM user_buy_game WHERE user_buy_game={id_purchase} LIMIT 1"
             purchase_data = db_manager.get_data(query_purchase)
+            
             if purchase_data:
                 user_balance_id=purchase_data[0][0]
                 amount= purchase_data[0][1]
                 transaction_date=purchase_data[0][2]
+            
             values.append((amount, description, transaction_date, payment_type, user_balance_id))
-    db_manager.executemany_query(query, values)
+    db_manager.executemany_query(query_insert_balance, values)
--- script/librerias.py
+++ script/librerias.py
@@ -3,15 +3,3 @@ fake = Faker()
 import random
 from datetime import datetime, timedelta, date
 import time
-from database_manager import DatabaseManager
-from user import insert_into_user,insert_into_developer
-from game import insert_into_game,insert_into_user_buy_game
-from event import insert_into_event
-from game_event import insert_into_game_event
-from review_game import insert_into_game_review
-from community import insert_into_community
-from community_user import insert_into_community_user
-from gift import insert_into_gift
-from items import insert_into_items, insert_into_products
-from rarity import insert_into_rarity
-from balance import insert_into_balance
